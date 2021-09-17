8d9ba9cff3045d847acebba93b1c2a40b3a27ce1
polyushina402 <polyushina.dana@mail.ru>
2021-09-17 18:57:54 +0300

    game

--- src/View.php
+++ src/View.php
@@ -1,7 +1,21 @@
 <?php
 	namespace polyushina402\hangman\View;
 	
-	function showGame()
+	function showGame($faultCount, $progress)
 	{
-		echo "Work my job".PHP_EOL;
+		//псведографика
+    $graphic = array (
+        " +---+\n     |\n     |\n     |\n    ===\n ",
+        " +---+\n 0   |\n     |\n     |\n    ===\n ",
+        " +---+\n 0   |\n |   |\n     |\n    ===\n ",
+        " +---+\n 0   |\n/|   |\n     |\n    ===\n ",
+        " +---+\n 0   |\n/|\  |\n     |\n    ===\n ",
+        " +---+\n 0   |\n/|\  |\n/    |\n    ===\n ",
+        " +---+\n 0   |\n/|\  |\n/ \  |\n    ===\n "
+    );
+
+    \cli\line($graphic[$faultCount]);
+    \cli\line($progress);
+
+    echo "\n";
 	}
