893b5ec2bc3147f2f29e6932aa2eb6abf66908ed
polyushina402 <polyushina.dana@mail.ru>
2021-09-24 18:06:44 +0300

    task03

--- src/View.php
+++ src/View.php
@@ -5,18 +5,18 @@ namespace polyushina402\hangman\View;
 function showGame($faultCount, $progress)
 {
 //псведографика
- $graphic = array (
- " +---+\n     |\n     |\n     |\n    ===\n ",
- " +---+\n 0   |\n     |\n     |\n    ===\n ",
- " +---+\n 0   |\n |   |\n     |\n    ===\n ",
- " +---+\n 0   |\n/|   |\n     |\n    ===\n ",
- " +---+\n 0   |\n/|\  |\n     |\n    ===\n ",
- " +---+\n 0   |\n/|\  |\n/    |\n    ===\n ",
- " +---+\n 0   |\n/|\  |\n/ \  |\n    ===\n "
- );
+    $graphic = array (
+    " +---+\n     |\n     |\n     |\n    ===\n ",
+    " +---+\n 0   |\n     |\n     |\n    ===\n ",
+    " +---+\n 0   |\n |   |\n     |\n    ===\n ",
+    " +---+\n 0   |\n/|   |\n     |\n    ===\n ",
+    " +---+\n 0   |\n/|\  |\n     |\n    ===\n ",
+    " +---+\n 0   |\n/|\  |\n/    |\n    ===\n ",
+    " +---+\n 0   |\n/|\  |\n/ \  |\n    ===\n "
+    );
 
- \cli\line($graphic[$faultCount]);
- \cli\line($progress);
+    \cli\line($graphic[$faultCount]);
+    \cli\line($progress);
 
- echo "\n";
+    echo "\n";
 }
